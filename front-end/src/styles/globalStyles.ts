fd5e801f925078c5f41ad10734ea4129214a5493
vinnidias <vinicius.3991@gmail.com>
2022-07-13 17:28:20 -0300

    adding some font-family to global styles

--- front-end/src/styles/globalStyles.ts
+++ front-end/src/styles/globalStyles.ts
@@ -10,5 +10,9 @@ export const GlobalStyles = createGlobalStyle`
   body {
     min-height: 100vh;
     overflow-x: hidden;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  
   }
 `;
