56d9909ab3806205d06e46509325d54fced10fed
vinnidias <vinicius.3991@gmail.com>
2022-07-13 17:28:37 -0300

    first testing on animated text component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { AnimatedTyper } from './components/AnimatedTyper';
 
 
 function App() {
+  const wordsList = [
+    "Teste 1",
+    2000,
+    "Teste 2",
+    2000,
+    "Teste 3",
+    2000,
+    "Teste 4",
+    2000,
+  ]
+
   return (
-    <div>
-      <h2>Desafio Impulso Gov</h2>
-    </div>
+    <AnimatedTyper wordsList={wordsList} />
   );
 }
 
