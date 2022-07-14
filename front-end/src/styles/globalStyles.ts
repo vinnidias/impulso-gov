648254c872da4d5954a91ae7962405454dc61f2d
vinnidias <vinicius.3991@gmail.com>
2022-07-14 11:42:48 -0300

    creating a custom body style

--- front-end/src/styles/globalStyles.ts
+++ front-end/src/styles/globalStyles.ts
@@ -10,9 +10,32 @@ export const GlobalStyles = createGlobalStyle`
   body {
     min-height: 100vh;
     overflow-x: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
+
+    ::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(45deg, #fff, #fff 50%, #162c3b 50%, #162c3b);
+    }
+
+    ::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(160deg, #3475ca, #3475ca 50%, transparent 50%, transparent);
+    }
   
   }
 `;
