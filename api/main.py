7994870de09292d931ec36ead1a37d15172da7b1
vinnidias <vinicius.3991@gmail.com>
2022-07-14 11:59:14 -0300

    cors setup and removing second on current time

--- api/main.py
+++ api/main.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],  
+)
+
+
 @app.get("/")
 def server_is_running():
-  return {
-    "message": "server is running"
-  }
+    return {
+        "message": "server is running"
+    }
+
 
 @app.get("/current_time")
 def current_time():
-  time_now =  datetime.now().strftime("%H:%M:%S")
-  return {
-    "current_time": time_now
-  }
+    time_now = datetime.now().strftime("%H:%M")
+    return {
+        "current_time": time_now
+    }
